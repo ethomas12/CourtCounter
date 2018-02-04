5f41f1692caa43b1091508fce47d9946c02cb16b
Elaine Thomas <el41net@gmail.com>
2018-02-04 16:28:36 -0700

    basic functionality working

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,7 +7,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+//............Team A Methods
     /**
      * Displays the given score for Team A.
      */
@@ -26,32 +28,57 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method increments the score by 1 when the Free Throw button is clicked.
      */
-    public void freeThrow(View view) {
-        score = score + 1;
-        displayForTeamA(score);
+    public void freeThrowTeamA(View view) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
      * This method decrements the score by 2 when the + 2 Points is clicked.
      */
-    public void twoPoints(View view) {
-        score = score + 2;
-        displayForTeamA(score);
+    public void twoPointsTeamA(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
      * This method decrements the score by 3 when the + 3 Points is clicked.
      */
-    public void threePoints(View view) {
-        score = score + 3;
-        displayForTeamA(score);
+    public void threePointsTeamA(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+//................Team B Methods
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
     }
 
     /**
-     * This method displays the given text on the screen.
+     * This method increments the score by 1 when the Free Throw button is clicked.
      */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.team_a_score);
-        priceTextView.setText(message);
+    public void freeThrowTeamB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method decrements the score by 2 when the + 2 Points is clicked.
+     */
+    public void twoPointsTeamB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method decrements the score by 3 when the + 3 Points is clicked.
+     */
+    public void threePointsTeamB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
     }
 }
