f559f7af660e3bac9901624a897db0b558939ef0
Elaine Thomas <el41net@gmail.com>
2018-05-09 21:11:59 -0600

    figured out the image buttons with the scoring, oversize image has been reduced so it doesn't bork the display

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -3,25 +3,88 @@ package com.example.android.courtcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    //implement the onClick method here
+    public void onClick(View v) {
+
+        ImageButton teama3 = findViewById(R.id.teama3); // Team A 3 points
+        teama3.setOnClickListener(this);
+
+        ImageButton teama2 = findViewById(R.id.teama2); //Team A 2 points
+        teama2.setOnClickListener(this);
+
+        ImageButton teama1 = findViewById(R.id.teama1); //Team A 1 point
+        teama1.setOnClickListener(this);
+
+        ImageButton teamb3 = findViewById(R.id.teamb3); //Team B 3 points
+        teamb3.setOnClickListener(this);
+
+        ImageButton teamb2 = findViewById(R.id.teamb2); //Team B 2 points
+        teamb2.setOnClickListener(this);
+
+        ImageButton teamb1 = findViewById(R.id.teamb1); //Team B 1 point
+        teamb1.setOnClickListener(this); }
+
+
+        // Perform action on click
+//        switch (v.getId()) {
+//
+//            case R.id.teama3:
+//                scoreTeamA = scoreTeamA + 3;
+//                displayForTeamA(scoreTeamA);
+//                break;
+//            case R.id.teama2:
+//                scoreTeamA = scoreTeamA + 2;
+//                displayForTeamA(scoreTeamA);
+//                break;
+//            case R.id.teama1:
+//                scoreTeamA = scoreTeamA + 1;
+//                displayForTeamA(scoreTeamA);
+//                break;
+//
+//
+//            case R.id.teamb3:
+//                scoreTeamB = scoreTeamB + 3;
+//                displayForTeamB(scoreTeamB);
+//                break;
+//            case R.id.teamb2:
+//                scoreTeamB = scoreTeamB + 2;
+//                displayForTeamB(scoreTeamB);
+//            case R.id.teamb1:
+//                scoreTeamB = scoreTeamB + 1;
+//                displayForTeamB(scoreTeamB);
+//
+//        }
+//    }
+
+
+
+
 //............Team A Methods
-    /**
-     * Displays the given score for Team A.
-     */
+        /**
+         * Displays the given score for Team A.
+         */
+
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
@@ -50,11 +113,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 //................Team B Methods
+
     /**
      * Displays the given score for Team B.
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        TextView scoreView = findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
